93882d437832ba5eb6584e9976104870d7c614ef
Jonathan-Greve <jonagrev@gmail.com>
2023-01-05 15:36:48 +0800

    Added new colors using chatGPT

--- ChatWriter.h
+++ ChatWriter.h
@@ -4,15 +4,41 @@ enum ChatColor
 {
     White,
     Black,
-    DarkRed,
     Red,
-    LightRed,
-    DarkBlue,
+    Yellow,
     Blue,
-    LightBlue,
-    DarkGreen,
+    Purple,
     Green,
-    LightGreen
+    Orange,
+    Pink,
+    LightRed,
+    LightYellow,
+    LightBlue,
+    LightPurple,
+    LightGreen,
+    LightOrange,
+    LightPink,
+    DarkRed,
+    DarkYellow,
+    DarkBlue,
+    DarkPurple,
+    DarkGreen,
+    DarkOrange,
+    DarkPink,
+    VeryLightRed,
+    VeryLightYellow,
+    VeryLightBlue,
+    VeryLightPurple,
+    VeryLightGreen,
+    VeryLightOrange,
+    VeryLightPink,
+    VeryDarkRed,
+    VeryDarkYellow,
+    VeryDarkBlue,
+    VeryDarkPurple,
+    VeryDarkGreen,
+    VeryDarkOrange,
+    VeryDarkPink
 };
 
 class ChatWriter
@@ -28,32 +54,110 @@ class ChatWriter
         case Black:
             hex_color = "#000000";
             break;
-        case DarkRed:
-            hex_color = "#7B241C";
-            break;
         case Red:
-            hex_color = "#A93226";
+            hex_color = "#FF4136";
             break;
-        case LightRed:
-            hex_color = "#E6B0AA";
-            break;
-        case DarkBlue:
-            hex_color = "#1A5276";
+        case Yellow:
+            hex_color = "#FFDC00";
             break;
         case Blue:
-            hex_color = "#2980B9";
+            hex_color = "#0074D9";
             break;
-        case LightBlue:
-            hex_color = "#A9CCE3";
-            break;
-        case DarkGreen:
-            hex_color = "#196F3D";
+        case Purple:
+            hex_color = "#B10DC9";
             break;
         case Green:
-            hex_color = "#28B463";
+            hex_color = "#2ECC40";
+            break;
+        case Orange:
+            hex_color = "#FF851B";
+            break;
+        case Pink:
+            hex_color = "#F012BE";
+            break;
+        case LightRed:
+            hex_color = "#FF7F50";
+            break;
+        case LightYellow:
+            hex_color = "#FFF8DC";
+            break;
+        case LightBlue:
+            hex_color = "#87CEEB";
+            break;
+        case LightPurple:
+            hex_color = "#DDA0DD";
             break;
         case LightGreen:
-            hex_color = "#ABEBC6";
+            hex_color = "#90EE90";
+            break;
+        case LightOrange:
+            hex_color = "#FFA07A";
+            break;
+        case LightPink:
+            hex_color = "#FFC0CB";
+            break;
+        case DarkRed:
+            hex_color = "#8B0000";
+            break;
+        case DarkYellow:
+            hex_color = "#9B870C";
+            break;
+        case DarkBlue:
+            hex_color = "#00008B";
+            break;
+        case DarkPurple:
+            hex_color = "#4B0082";
+            break;
+        case DarkGreen:
+            hex_color = "#006400";
+            break;
+        case DarkOrange:
+            hex_color = "#FF8C00";
+            break;
+        case DarkPink:
+            hex_color = "#FF1493";
+            break;
+        case VeryLightRed:
+            hex_color = "#FFF8DC";
+            break;
+        case VeryLightYellow:
+            hex_color = "#FFFFF0";
+            break;
+        case VeryLightBlue:
+            hex_color = "#F0FFFF";
+            break;
+        case VeryLightPurple:
+            hex_color = "#F5F0FF";
+            break;
+        case VeryLightGreen:
+            hex_color = "#F0FFF0";
+            break;
+        case VeryLightOrange:
+            hex_color = "#FFECB3";
+            break;
+        case VeryLightPink:
+            hex_color = "#FFF8DC";
+            break;
+        case VeryDarkRed:
+            hex_color = "#800000";
+            break;
+        case VeryDarkYellow:
+            hex_color = "#964B00";
+            break;
+        case VeryDarkBlue:
+            hex_color = "#000080";
+            break;
+        case VeryDarkPurple:
+            hex_color = "#663399";
+            break;
+        case VeryDarkGreen:
+            hex_color = "#003300";
+            break;
+        case VeryDarkOrange:
+            hex_color = "#FF7F00";
+            break;
+        case VeryDarkPink:
+            hex_color = "#FF00FF";
             break;
         }
         return hex_color;
